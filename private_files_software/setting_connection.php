2e72da22399ad84914de51d4574bef7baeb993aa
mariacer13 <143567571+mariacer13@users.noreply.github.com>
2024-06-12 15:18:07 -0500

    Update setting_connection.php

--- private_files_software/setting_connection.php
+++ private_files_software/setting_connection.php
@@ -1,11 +1,11 @@
 <?php
     //Valores de la base datos
-    const IP = 'localhost';
+    const IP = 'odontok.azurewebsites.net';
     const DATA_BASE = 'odontok'; // Nombre de la base de datos
     const PORT = '5432';
 
     //Credenciales de acceso
-    const USER_POSTGRES = 'postgres'; //Usuario
-    const PASSWORD_POSTGRES = '123456'; //Contraseña del usuario
+    const USER_POSTGRES = 'administrador'; //Usuario
+    const PASSWORD_POSTGRES = '123456ML#'; //Contraseña del usuario
 
 ?>
